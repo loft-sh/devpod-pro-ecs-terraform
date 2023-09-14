5fff9093b20b279c1a82fd5f74f0ffb85f5d0ab8
Hernan <hernaner28@gmail.com>
2023-09-14 11:12:39 +0000

    Add task role

--- iam.tf
+++ iam.tf
@@ -26,8 +26,25 @@ resource "aws_iam_policy" "iam_policy" {
   })
 }
 
-# Create an IAM role
-resource "aws_iam_role" "iam_role" {
+# Create both Task and Execution roles
+resource "aws_iam_role" "task_exec_role" {
+  name = "${var.name_prefix}-iam-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "task_role" {
   name = "${var.name_prefix}-iam-role"
 
   assume_role_policy = jsonencode({
@@ -48,15 +65,15 @@ resource "aws_iam_role" "iam_role" {
 resource "aws_iam_policy_attachment" "iam_role_policy_attachment" {
   name = "Policy Attachement"
   policy_arn = aws_iam_policy.iam_policy.arn
-  roles      = [aws_iam_role.iam_role.name]
+  roles      = [aws_iam_role.task_exec_role.name]
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_full_access_attachment" {
-  role       = aws_iam_role.iam_role.name
+resource "aws_iam_role_policy_attachment" "ecs_full_access" {
+  role       = aws_iam_role.task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "iam_read_only_access_attachment" {
-  role       = aws_iam_role.iam_role.name
+resource "aws_iam_role_policy_attachment" "iam_read_only_access" {
+  role       = aws_iam_role.task_role.name
   policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
 }
