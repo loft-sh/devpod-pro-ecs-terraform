364485ba2c90037c8b7526c70bfa07532ea5224e
Hernan <hernaner28@gmail.com>
2023-09-14 10:21:07 +0000

    Add ECS Full access and IAM Read only to ECS task IAM role

--- iam.tf
+++ iam.tf
@@ -50,3 +50,13 @@ resource "aws_iam_policy_attachment" "iam_role_policy_attachment" {
   policy_arn = aws_iam_policy.iam_policy.arn
   roles      = [aws_iam_role.iam_role.name]
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_full_access_attachment" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_read_only_access_attachment" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+}
